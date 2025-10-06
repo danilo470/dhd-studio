83e622e30d4cf09138935eba3ae181cd37fc1973
danilo470 <stankovicdanilo7@gmail.com>
2025-10-06 14:39:19 +0200

    g
    g

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,15 @@
 const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("/photo-images");
+  eleventyConfig.addPassthroughCopy("dhd-images");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("blog.css");
   eleventyConfig.addPassthroughCopy("script.js");
   
 
-  // Add the watch target for your sections folder
+  // // Add the watch target for your sections folder
   eleventyConfig.addWatchTarget("/_includes/sections");
+  eleventyConfig.addWatchTarget("../tailwind.css");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
@@ -16,7 +17,7 @@ module.exports = function (eleventyConfig) {
    return {
     dir: {
       input: ".",      // assuming you're working from root
-      output: "_site"  // default output folder
+      output: "/_site"  // default output folder
     }
   };
 };
